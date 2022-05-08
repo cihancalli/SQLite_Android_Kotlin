4fc5d326876ef04a96f04b2098f0fbf67bfc8c6c
cihancalli <cihan.callii@gmail.com>
2022-05-08 21:12:06 +0300

    SQL insert

--- app/src/main/java/com/zerdasoftware/sqlite/MainActivity.kt
+++ app/src/main/java/com/zerdasoftware/sqlite/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val dbHelper = DBHelper(this)
+
+        var persons = Persons(1,
+            "Cihan",
+            "05350000000",
+            21,
+            1.75)
+
+        PersonsDAO().PersonAdd(dbHelper,persons)
     }
 }
--- app/src/main/java/com/zerdasoftware/sqlite/PersonsDAO.kt
+++ app/src/main/java/com/zerdasoftware/sqlite/PersonsDAO.kt
@@ -1,8 +1,18 @@
 package com.zerdasoftware.sqlite
 
+import android.content.ContentValues
+
 class PersonsDAO {
 
-    fun PersonAdd(){
+    fun PersonAdd(dbHelper:DBHelper,persons: Persons){
+        val db = dbHelper.writableDatabase
+        val values = ContentValues()
+        values.put("person_name",persons.person_name)
+        values.put("person_number",persons.person_number)
+        values.put("person_age",persons.person_age)
+        values.put("person_height",persons.person_height)
 
+        db.insertOrThrow("persons",null,values)
+        db.close()
     }
 }
